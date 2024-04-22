1fcbe617b51dafe853bb0c8790d76ef9e3bbf1c9
IJN Akashi AR <dabanya@protonmail.com>
2024-04-21 17:45:38 -0700

    edit

--- publish.sh
+++ publish.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
 
 Help() {
-  printf "build"
+	printf "build"
 }
-
-while getopts ":hd" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-      d)
-
-      *)
-        exit;;
-   esac
-done
 
 root_dir=$(pwd)
 build_dir=$root_dir/build
 site_dir=$root_dir/site
 dist_dir=$root_dir/dist
 
+# shellcheck source=/dev/null
 . "$root_dir"/.env
 
 rm -rf "${build_dir:?}"
@@ -28,16 +17,28 @@ mkdir "$build_dir"
 rm -rf "${dist_dir:?}"
 mkdir "$dist_dir"
 
-cp -R "${site_dir:?}"/* "$build_dir"
+while getopts ":hd" option; do
+	case $option in
+		h) # display Help
+			Help
+			exit;;
+		d)
+			cp -Rv "${site_dir:?}"/* "$dist_dir"
+			yarn parcel build "${dist_dir}/*.html" --config .parcelrc_dev --no-cache --dist-dir "dist"
+			# cp -R "$root_dir"/site/scripts/ "$build_dir"
+		;;
 
-yarn parcel build "$build_dir/*.html" --no-source-maps --no-cache
+		*)
+			yarn parcel build "${build_dir}/*.html" --no-source-maps --no-cache
+			cp -R "${site_dir:?}"/* "$build_dir"
 
-# Static content
-rsync -av -e ssh --delete "$root_dir"/dist/ "${static_dest:?}"
+			# Static content
+			rsync -av -e ssh --delete "$root_dir"/dist/ "${static_dest:?}"
 
-# Scripts
-rsync -av -e ssh --delete "$root_dir"/site/scripts/ "${scripts_dest:?}"
+			# Scripts
+			rsync -av -e ssh --delete "$root_dir"/site/scripts/ "${scripts_dest:?}"
 
-# Protected directory, for non user-facing data
-rsync -av -e ssh "$root_dir"/site/protected/
-
+			# Protected directory, for non user-facing data
+			rsync -av -e ssh "$root_dir"/site/protected/
+	esac
+done
