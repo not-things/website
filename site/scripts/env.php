ad47fec228205406a9efd6249bc90f07313742fd
IJN Akashi AR <gcciscool@protonmail.com>
2024-05-04 14:29:41 -0700

    minor

--- site/scripts/env.php
+++ site/scripts/env.php
@@ -1,7 +1,7 @@
 <?php
 
-//const DATA_DIR = '/home/protected';
-//const CONFIG_DIR = '/home/protected';
+const DATA_DIR = '/home/protected';
+const CONFIG_DIR = '/home/protected';
 
-define("DATA_DIR", getcwd() . '/../protected/');
-define("CONFIG_DIR", getcwd() . '/../protected/');
+//define("DATA_DIR", getcwd() . '/../protected/');
+//define("CONFIG_DIR", getcwd() . '/../protected/');
